b01bbef2957b9e872132633ba8e3eec112a18f1c
adarshrwl <adarshrawal321@gmail.com>
2023-07-13 08:11:12 +0545

    complete login and signup

---
+++ Pages/Login.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("Login");
+var y = document.getElementById("Register");
+var z = document.getElementById("btn");
+
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
